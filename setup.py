9035243a7429cb4b00ab7528e910b009c52ecef3
Holden Karau <holden@pigscanfly.ca>
2014-06-07 16:57:49 -0700

    switch to README.md

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url='https://github.com/holdenk/PandaSpark',
     license='LICENSE.txt',
     description='Enable Pandas on PySpark',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     install_requires=[
         # Note: we also need PySpark but that has to be installed manually.
         'pandas >= 0.13',
