c05736bdf98aedcbca2eb53979d7797847b4e3ad
Holden Karau <holden@pigscanfly.ca>
2014-07-11 11:18:21 -0700

    Remove extra space from docstring

--- sparklingpandas/groupby.py
+++ sparklingpandas/groupby.py
@@ -1,6 +1,4 @@
-"""
-Provide wrapper around the grouped result from L{PRDD}
-"""
+"""Provide wrapper around the grouped result from L{PRDD}"""
 #
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
