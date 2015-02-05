69323f071e06a9520d9c36d1f51f5a89f5d17e1c
Holden Karau <holden@pigscanfly.ca>
2015-02-04 17:43:32 -0800

    Don't fail hard if we can't read the README

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
+try:
+    import pypandoc
+    readme = pypandoc.convert('README.md', 'rest')
+except (ImportError, IOError):
+    print "Failed to import pypandoc or failed to load README. Using empty readme"
+    readme = ""
 setup(
     name='sparklingpandas',
     version='0.0.1',
@@ -9,12 +15,13 @@ setup(
     url='https://github.com/holdenk/PandaSpark',
     license='LICENSE.txt',
     description='Enable Pandas on PySpark',
-    long_description=open('README.md').read(),
+    long_description=readme,
     install_requires=[
         # Note: we also need PySpark 1.2 but that has to be installed manually.
         'pandas >= 0.13',
         'openpyxl>=1.6.1,<=2.0.0',
-        'py4j'
+        'py4j',
+        'pypandoc'
     ],
     test_requires=[
         'nose',
