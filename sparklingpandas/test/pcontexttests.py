4d66dae485105acbc123a6be01f1c720254cd774
Holden Karau <holden@pigscanfly.ca>
2015-04-03 22:08:33 -0700

    sort both sides in the test

--- sparklingpandas/test/pcontexttests.py
+++ sparklingpandas/test/pcontexttests.py
@@ -29,4 +29,4 @@ class PContextTests(SparklingPandasTestCase):
         prdd = self.psc.DataFrame(input, columns=['magic', 'thing'])
         elements = prdd.collect()
         assert len(elements) == 3
-        assert sorted(elements['magic']) == [u'coffee', u'tea', u'water']
+        assert sorted(elements['magic']) == sorted([u'coffee', u'tea', u'water'])
