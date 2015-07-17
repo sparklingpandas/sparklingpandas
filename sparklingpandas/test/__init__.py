ba012fcf529b6b8881eaba2521c33f04618ae1a5
Juliet Hougland <not@myemail.com>
2015-07-17 17:17:08 -0500

    Change how we setup test env and run tests.

--- sparklingpandas/test/__init__.py
+++ sparklingpandas/test/__init__.py
@@ -2,3 +2,13 @@
 Tests for the PandaSpark module, you probably don't want to import this
 directly.
 """
+import subprocess as sub
+
+try:
+    sub.check_output("source ./sparklingpandas/test/resources/setup-test-env"
+                     + ".sh", shell=True,
+                        stderr=sub.STDOUT)
+except sub.CalledProcessError, err:
+        print "The failed test setup command was [%s]." % err.cmd
+        print "The output of the command was [%s]" % err.output
+        raise
---
+++ sparklingpandas/test/resources/setup-test-env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -z "$SPARK_HOME" ]; then
+   echo "$(tput setaf 1)Error: SPARK_HOME is not set, can not run tests.$(tput sgr0)"
+   exit -1
+fi
+
+JARS=`ls ./target/scala-2.10/*.jar`
+PYSPARK_SUBMIT_ARGS="--jars $JARS --driver-class-path $JARS pyspark-shell"
+PYTHON_PATH=./
